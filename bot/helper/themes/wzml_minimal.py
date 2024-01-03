c3376d87ab96dc529ad4a34029c18c80f56b3791
vikas-843 <119723483+vikas-843@users.noreply.github.com>
2024-01-03 12:13:11 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,14 +2,16 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://www.github.com/weebzone/WZML-X'
-    ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/WZML_X'
+    ST_BN1_NAME = 'updates'
+    ST_BN1_URL = 'https://t.me/TEAMS_VG'
+    ST_BN2_NAME = '🤖Use Me'
+    ST_BN2_URL = 'https://t.me/mirror_leech_group4gb'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
-    ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
+    ST_UNAUTH = '''<i>Bot PM Initiated Successfully!
+
+I will send all your files and links here.</i>'''
     OWN_TOKEN_GENERATE = '''<b>Temporary Token is not yours!</b>\n\n<i>Kindly generate your own.</i>'''
     USED_TOKEN = '''<b>Temporary Token already used!</b>\n\n<i>Kindly generate a new one.</i>'''
     LOGGED_PASSWORD = '''<b>Bot Already Logged In via Password</b>\n\n<i>No Need to Accept Temp Tokens.</i>'''
